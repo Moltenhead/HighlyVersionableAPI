934d7ed15aa42ff05be84b9a6386330935fadc7f
Moltenhead <ectopneo@gmail.com>
2021-11-18 13:23:09 +0100

    feat: add iban model

---
+++ app/models/iban.rb
@@ -0,0 +1,4 @@
+class Iban < ApplicationRecord
+  # Controlls are fully handled by the controller for higher versionability
+  # - this can still serve if some behaviours must be ensured within any versions
+end
