0407b7a0248ffe423738fe0fd908adc218dbfdc5
Moltenhead <ectopneo@gmail.com>
2021-11-18 13:20:47 +0100

    rm: debug

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,15 +1,14 @@
 RSpec.configure do |config|
-  print "in"
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
 
   %i[each all].each do |sym|
+    config.before(sym, :js => true) { DatabaseCleaner.strategy = :truncation }
     config.before(sym) do
       DatabaseCleaner.strategy = :transaction
       DatabaseCleaner.start
     end
-    config.beofre(sym, :js => true) { DatabaseCleaner.strategy = :truncation }
     config.after(sym) { DatabaseCleaner.clean }
   end
 end
