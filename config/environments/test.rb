e248de1f5ed7792eae4c53ef0818c8a28bbb8e93
Moltenhead <ectopneo@gmail.com>
2021-11-18 13:17:15 +0100

    fix & rm: docker host trouble & database_cleaner require in test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,4 @@
 require "active_support/core_ext/integer/time"
-require 'database_cleaner'
 
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
@@ -8,6 +7,7 @@ require 'database_cleaner'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "www.example.com"
 
   config.cache_classes = false
   config.action_view.cache_template_loading = true
